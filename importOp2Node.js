cb27e5299b9c26c5a2d544c698834fe67f8dcd78
cshashank <chitnis.shashank@gmail.com>
2019-02-25 23:53:30 -0500

    fixed typo for firstName

--- importOp2Node.js
+++ importOp2Node.js
@@ -2,5 +2,5 @@ WITH "file:///op2_node.csv"
 AS uri
 LOAD CSV WITH HEADERS FROM uri  AS row
 MERGE (employee:Employee {id:row.id})
-SET employee.firstName = row.frist_name,
-	employee.firstName = row.frist_name
+SET employee.firstName = row.first_name,
+	employee.lastName = row.last_name
